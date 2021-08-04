1cbbbe75fa6f6eff0a77e70a9aff020c05d0a900
only-solutions <75507583+only-solutions@users.noreply.github.com>
2021-08-04 18:12:34 -0500

    Add subtotals by item type

--- IdeaProjects/processdata/README.md
+++ IdeaProjects/processdata/README.md
@@ -13,9 +13,11 @@ https://cursive-ide.com/userguide/
 
 Open file in IntelliJ with the Cursive plugin
 1. Open project
-2. Right click on project.clj and Run Repl
-3. Edit path to testdata.txt in IdeaProjects/processdata/src/processdata/core.clj file.  Save.  Right click in editor, REPL -> Load File in REPL
-4. Run (-main) in window below REPL
+2. Open File -> Project Structure -> Project
+3. Select Java SDK to use.  Click Apply & OK
+4. Right click on project.clj and Run Repl
+5. Edit path to testdata.txt in IdeaProjects/processdata/src/processdata/core.clj file.  Save.  Right click in editor, REPL -> Load File in REPL
+6. Run (-main) in window below REPL
 
 ## Options
 
--- IdeaProjects/processdata/src/processdata/core.clj
+++ IdeaProjects/processdata/src/processdata/core.clj
@@ -1,38 +1,59 @@
 (ns processdata.core
   (:gen-class)
   (:require [clojure.string :as str]))
-
 (use 'clojure.java.io)
-(require ['clojure.string :as 'str])
 
 (def readfile (fn [filename]
                  (with-open [r (reader filename)]
                    (str/split-lines (slurp filename)))))
 
 (defn getcolfromline [line col]
-  ( (str/split line #" ") col)
+           ((str/split line #" ") col)
   )
+
+;;(mapv #(= (str (first %)) "a") contents)
+;;(filterv #(= (str (first %)) "a") contents)
+(defn filterlines [col filterstr data]
+  (filterv #(= (str (first %)) filterstr) data)
+  )
+
 
 (defn getcol [col data]
   (map #(Integer/parseInt %)
        (map #(getcolfromline % col) data) )
   )
 
+(defn getcolstr [col data]
+       (map #(getcolfromline % col) data)
+  )
+
 (defn sumcoln [contents x]
   (let [
         colsum (reduce + (into [] (getcol x contents)))
         ]
-    (println "Column " x " sum is " colsum)
+    colsum
+    ;;(println "Column " x " sum is " colsum)
   )
   )
 
+(defn itemtypes [contents]
+  (apply sorted-set (set (into [] (getcolstr 0 contents))))
+)
+
 (defn -main
-  "Sum columns 1 and 2 in input data file"
+  "Find unique item types in data file and sub-total columns 1 and 2"
   [& args]
   (def contents (readfile "/home/there/testdata.txt"))
   (def colstoprocess [1 2])
+  ;;determine unique types in column 0
+  (def itemtypesvec (itemtypes contents))
 
-  (for [x colstoprocess]
-    (sumcoln contents x)
-    )
+  ;;x is a, b, c
+  ;;y is 1,2
+  (for [x itemtypesvec y colstoprocess
+       :let [linesofitemtype (filterlines 0 x contents)
+            sumofitemtype (sumcoln linesofitemtype y )]
+        ]
+       (println "Type " x "Column " y " sum is " sumofitemtype)
+       )
   )
